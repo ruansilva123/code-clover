54e229988ff63f9fa243329d999f3710f08b2174
Ruan Silva <ruan.silva@livon.io>
2026-05-09 14:29:08 -0300

    feat: add basic git log

--- git_actions/git_log.py
+++ git_actions/git_log.py
@@ -1,2 +1,18 @@
+import requests as req
+
 if __name__ == "__main__":
-    pass
+    try:
+        print("Enter the repository ID: ")
+        repo_id = int(input())
+
+        response = req.get(
+            url=f'http://localhost:3000/repositories/{repo_id}',
+        )
+
+        commits = response.json()['commits']
+
+        print("Commit List:")
+        for commit in commits:
+            print(f"{commit['id']} - {commit['message']}")
+    except Exception as e:
+        print(f"Error get repository logs. Error: {e}")
