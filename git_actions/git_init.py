71c921e63b34a6813af9c4d8eea8a1b72abe950c
Ruan Silva <ruan.silva@livon.io>
2026-05-09 15:02:40 -0300

    feat: add git commit

---
+++ git_actions/git_init.py
@@ -0,0 +1,21 @@
+import requests as req
+
+if __name__ == "__main__":
+    try:
+        print("Enter the user ID: ")
+        user_id = int(input())
+
+        print("Enter the repository name: ")
+        name = str(input())
+
+        req.post(
+            url='http://localhost:3000/commits',
+            json={
+                "name": name,
+                "user_id": user_id
+            }
+        )
+
+        print("Repository created in origin...")
+    except Exception as e:
+        print(f"Error to create repository. Error: {e}")
